80507b149b846bbfcb62435d5a9974583f1d48c3
tarunsilam07 <tarun79767@gmail.com>
2024-11-01 09:51:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Swap and Maximize/swap-and-maximize.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// long long int maxSum(int arr[], int n);
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long maxSum(vector<int>& arr) {
+        vector<int>ans;
+        sort(arr.begin(),arr.end());
+        int i=0;
+        int n=arr.size();
+        int j=n-1;
+        while(i<=j){
+            if(i!=j){
+            ans.push_back(arr[i]);
+            ans.push_back(arr[j]);
+            }
+            if(i==j)
+            ans.push_back(arr[i]);
+            i++;
+            j--;
+        }
+        long long diff=0;
+        for(int i=1;i<n;i++){
+            diff+=abs(ans[i]-ans[i-1]);
+        }
+        diff+=abs(ans[n-1]-ans[0]);
+        return diff;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To ignore any newline character left in the buffer
+    while (t--) {
+        string line;
+        getline(cin, line); // Read the entire line of input
+        stringstream ss(line);
+        vector<int> arr;
+        int num;
+
+        // Parse the input line into integers and add to the vector
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        long long ans = ob.maxSum(arr);
+
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
