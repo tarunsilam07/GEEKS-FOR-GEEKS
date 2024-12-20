41729fe147c8e0a2c93a6a5fec535ee97c040f65
tarunsilam07 <tarun79767@gmail.com>
2024-12-20 11:37:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -1,14 +1,14 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
+
 
 // } Driver Code Ends
-class Solution
-{   
-    public: 
-    //Function to return a list of integers denoting spiral traversal of matrix.
-    vector<int> spirallyTraverse(vector<vector<int> > mat, int r, int c) 
-    {
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int> > &mat) {
+        int c=mat[0].size();
+        int r=mat.size();
         vector<int>ans;
         int top=0;
         int left=0;
@@ -38,28 +38,28 @@ class Solution
 //{ Driver Code Starts.
 int main() {
     int t;
-    cin>>t;
-    
-    while(t--) 
-    {
-        int r,c;
-        cin>>r>>c;
-        vector<vector<int> > matrix(r); 
+    cin >> t;
 
-        for(int i=0; i<r; i++)
-        {
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r);
+
+        for (int i = 0; i < r; i++) {
             matrix[i].assign(c, 0);
-            for( int j=0; j<c; j++)
-            {
-                cin>>matrix[i][j];
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
             }
         }
 
         Solution ob;
-        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        vector<int> result = ob.spirallyTraverse(matrix);
         for (int i = 0; i < result.size(); ++i)
-                cout<<result[i]<<" ";
-        cout<<endl;
+            cout << result[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
