14358dbd58d848103d408e0041bcf6410d9bcb47
tarunsilam07 <tarun79767@gmail.com>
2024-12-25 09:44:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+   void setMatrixZeroes(vector<vector<int>> &mat) {
+        // code here
+        set<int> r,c;
+        int n=mat.size(),m=mat[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    r.insert(i);c.insert(j);
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(r.count(i) || c.count(j)){
+                    mat[i][j]=0;
+                }
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > arr(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> arr[i][j];
+            }
+        }
+        Solution ob;
+        ob.setMatrixZeroes(arr);
+        for (auto x : arr) {
+            for (auto y : x)
+                cout << y << " ";
+            cout << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
