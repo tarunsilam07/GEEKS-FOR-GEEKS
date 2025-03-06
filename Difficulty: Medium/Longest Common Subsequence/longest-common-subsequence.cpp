0d4964b1ec0d50ce76a4e7f264aba1020b78ebe7
tarunsilam07 <tarun79767@gmail.com>
2025-03-06 18:58:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int lcs(string &s1, string &s2) {
+        // code here
+        int n=s1.length();
+        int m=s2.length();
+        vector<int>prev(m+1, 0), curr(m+1, 0);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s1[i-1]==s2[j-1]){
+                    curr[j]=1+prev[j-1];
+                }else curr[j]=max(prev[j], curr[j-1]);
+            }
+            prev=curr;
+        }
+        return prev[m];
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1, s2;
+        cin >> s1 >> s2; // Take both the strings as input
+        Solution ob;
+        cout << ob.lcs(s1, s2) << endl; // Call the lcs function
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
