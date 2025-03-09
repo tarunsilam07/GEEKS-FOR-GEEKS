b153da79298a13b949f1014c5e101470fd51f7d6
tarunsilam07 <tarun79767@gmail.com>
2025-03-09 18:30:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome SubStrings/palindrome-sub-strings.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int countPS(string &s) {
+        // code here
+         int n=s.length();
+        int res=0;
+        
+        //odd palindrome strings
+        for(int i=0;i<n;i++){
+            int l=i,r=i;
+            
+            while(l>=0 && r<n && s[l]==s[r]){
+                if(r-l+1>=2)
+                res++;
+                
+                l--;
+                r++;
+            }
+            
+            //even length palindromes
+             l=i,r=i+1;
+            
+            while(l>=0 && r<n && s[l]==s[r]){
+                if(r-l+1>=2)
+                res++;
+                
+                l--;
+                r++;
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.countPS(s) << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
