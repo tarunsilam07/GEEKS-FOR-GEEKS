3551be980417c5fef999eb7cc0db1e40125b7d5e
tarunsilam07 <tarun79767@gmail.com>
2025-04-05 19:57:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -2,45 +2,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 
 class Solution {
-public:
-    void dfs(vector<vector<char>>& grid, int i, int j, int n, int m) {
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == '0') {
-            return;
-        }
-        
-        grid[i][j] = '0';
-        
-        dfs(grid, i + 1, j, n, m);
-        dfs(grid, i - 1, j, n, m);
-        dfs(grid, i, j + 1, n, m);
-        dfs(grid, i, j - 1, n, m);
-        dfs(grid, i + 1, j + 1, n, m);
-        dfs(grid, i - 1, j - 1, n, m);
-        dfs(grid, i + 1, j - 1, n, m);
-        dfs(grid, i - 1, j + 1, n, m);
-    }
-
-    int numIslands(vector<vector<char>>& grid) {
+  public:
+    int countIslands(vector<vector<char>>& grid) {
+        int dx[8] = {0, 0, 1, -1, 1, 1, -1, -1};
+        int dy[8] = {1, -1, 0, 0, -1, 1, -1, 1};
         int n = grid.size();
         int m = grid[0].size();
-        int islandCount = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j, n, m);
-                    islandCount++;
+        int ans = 0;
+        for(int i= 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                
+                if(grid[i][j] == 'L'){
+                    queue<pair<int,int>>q; 
+                    q.push({i, j});
+                
+                    grid[i][j] = 'W';
+                    ans += 1;
+                
+                    while(!q.empty()){
+                        auto f = q.front();
+                        q.pop();
+                    
+                        for(int k = 0; k < 8; k++){
+                            int nx = f.first + dx[k];
+                            int ny = f.second + dy[k];
+                        
+                            if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 'L'){
+                                q.push({nx, ny});
+                                grid[nx][ny] = 'W';
+                            }
+                        }
+                    }
                 }
             }
         }
-
-        return islandCount;
+        return ans;
+        
     }
 };
-
 
 
 //{ Driver Code Starts.
@@ -57,8 +60,11 @@ int main() {
             }
         }
         Solution obj;
-        int ans = obj.numIslands(grid);
+        int ans = obj.countIslands(grid);
         cout << ans << '\n';
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
