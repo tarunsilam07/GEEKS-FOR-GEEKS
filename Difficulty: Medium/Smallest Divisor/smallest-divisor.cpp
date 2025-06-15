dff7298b59b4dc097b397dfaabdbe092594badd6
tarunsilam07 <tarun79767@gmail.com>
2025-06-15 21:45:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Divisor/smallest-divisor.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+      int smallestDivisor(vector<int>& arr, int k) {
+        int n = arr.size();
+        int low = 1;
+        int maxE = *max_element(arr.begin(), arr.end());
+        int high = maxE + k;
+        int ans = high;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int count = 0;
+            for(auto &e : arr){
+                count += ceil((double)e/mid);
+            }
+            if(count <= k){
+                ans = min(mid, ans);
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
