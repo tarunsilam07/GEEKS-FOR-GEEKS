fa750f832e487f955c74a0fb76254d073f724d3e
Silam Tarun <tarun79767@gmail.com>
2025-09-14 16:34:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.cpp
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -1,80 +1,21 @@
-//{ Driver Code Starts
-// C++ program to find union of
-// two sorted arrays
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
-/*You are required to complete this method*/
 class Solution {
   public:
     int startStation(vector<int> &gas, vector<int> &cost) {
-        // Your code here
-        
-        int totalgas = 0;
-        int totalcost = 0;
-        int currgas = 0;
-        int start = 0;
-        
-        for(int i=0; i<gas.size(); i++)
-        {
-            totalgas += gas[i];
-            totalcost += cost[i];
-            
-            currgas += gas[i] - cost[i];
-            
-            if(currgas < 0)
-            {
-                start = i + 1;
-                currgas = 0;
+        //  code here
+        int sum1 = 0 , sum2 = 0;
+        for(auto i: gas)sum1+=i;
+        for(auto i: cost)sum2+=i;
+        if(sum2>sum1)return -1;   // check if cost is > the availagle gas
+        int sum =0 , ind =0;
+        for(int i=0;i<gas.size() ; i++){    // kadane algorithm on gas[i] - cost[i];
+            sum+= (gas[i] - cost[i]);
+            if(sum <0){
+                sum = 0;
+                ind = i+1;
             }
         }
+        return ind;
         
-        if(totalgas < totalcost)
-            return -1;
-        
-        else
-            return start;
     }
 };
 
-
-
-//{ Driver Code Starts.
-/* Driver program to test above function */
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-
-    while (t--) {
-
-        vector<int> gas, cost;
-        string input1;
-        getline(cin, input1);
-        stringstream ss(input1);
-        int number1;
-        while (ss >> number1) {
-            gas.push_back(number1);
-        }
-        string input2;
-        getline(cin, input2);
-        stringstream sss(input2);
-        int number2;
-        while (sss >> number2) {
-            cost.push_back(number2);
-        }
-        Solution ob;
-        int ans = ob.startStation(gas, cost);
-
-        cout << ans << endl;
-        cout << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
