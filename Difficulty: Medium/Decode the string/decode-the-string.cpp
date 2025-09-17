537d493cbe45a5c2ae2794e0bf94306c340956e0
Silam Tarun <tarun79767@gmail.com>
2025-09-17 20:30:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.cpp
+++ Difficulty: Medium/Decode the string/decode-the-string.cpp
@@ -1,68 +1,48 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
 class Solution {
   public:
-     string decodedString(string &s) {
-        //CodeGenius
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=']') st.push(s[i]);
-            else{
-                string word;
-                while(!st.empty() && st.top()!='['){
-                    word.push_back(st.top());
-                    st.pop();
+    string decodedString(string &s) {
+        // code here
+        stack<string>num;
+        string res="";
+        int c=0;
+        for(char ch:s )
+        {
+            if(ch==']')
+            {
+                string str="";
+                while(num.top()!="[")
+                {
+                   str=num.top()+str;
+                   num.pop();
                 }
-                reverse(word.begin(),word.end());//ishan
-                st.pop();
-                string k;
-                while(!st.empty() && isdigit(st.top())){
-                    k.push_back(st.top());
-                    st.pop();
+                num.pop();
+                res="";
+                res=str+res;
+                str=res;
+                string dig="";
+                while(!num.empty() && isdigit(num.top()[0]))
+                {
+                    dig=num.top()+dig;
+                    num.pop();
                 }
-                reverse(k.begin(),k.end());
-                int num=stoi(k);//3
-                string repeated;
-                for(int j=0;j<num;j++){
-                    repeated.append(word);//ishanishanishan
+                long long n= stoll(dig);
+                while(--n > 0)
+                {
+                     res+=str;
                 }
-                for(char c: repeated) st.push(c);
+                
+                num.push(res);
+            }else
+            {
+                num.push(string()+ch);
             }
         }
-        string ans;
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
+        num.pop();
+        while(!num.empty())
+        {
+            res=num.top()+res;
+            num.pop();
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return res;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-
-        Solution ob;
-        cout << ob.decodedString(s) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
